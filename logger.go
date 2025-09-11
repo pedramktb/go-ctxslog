1b65731e26b5ca3cb595ba1878a21c21fbed060e
pedramktb <79080845+pedramktb@users.noreply.github.com>
2025-09-11 19:28:43 +0200

    fix: copy slice on addition of attritues to avoid race conditions with slice aliases

--- logger.go
+++ logger.go
@@ -31,14 +31,16 @@ func FromContext(ctx context.Context) *slog.Logger {
 // WithAttrs adds attributes to the provided context.
 // Subsequent calls to the logger with this context (e.g. InfoContext()) will have the attributes added to them.
 func WithAttrs[T slog.Attr | func(context.Context, slog.Record) []slog.Attr](ctx context.Context, attrs ...T) context.Context {
-	attrsCtx, ok := ctx.Value(attrsCtxKey{}).([]any)
+	old, ok := ctx.Value(attrsCtxKey{}).([]any)
 	if !ok {
-		attrsCtx = make([]any, 0)
+		old = make([]any, 0)
 	}
+	new := make([]any, len(old), len(old)+len(attrs))
+	copy(new, old)
 	for i := range attrs {
-		attrsCtx = append(attrsCtx, attrs[i])
+		new = append(new, attrs[i])
 	}
-	return context.WithValue(ctx, attrsCtxKey{}, attrsCtx)
+	return context.WithValue(ctx, attrsCtxKey{}, new)
 }
 
 func attrs(ctx context.Context, r slog.Record) []slog.Attr {
